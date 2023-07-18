fbf18e6bf95217bb54135b2acf2a54d1667f5b6b
Shubham Saxena <68746176+Shubham-Saxena-dev@users.noreply.github.com>
2023-07-18 15:42:51 +0200

    initial structure

---
+++ RemindersApplication.java
@@ -0,0 +1,12 @@
+package com.mbition.hybridcloud.reminders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemindersApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RemindersApplication.class, args);
+  }
+}
